633a6996af9b7dbab9e896a821f54e9e0d5204e7
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-03-21 10:15:34 -0400

    Added `Services` abstractions.

---
+++ gello/services/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Service helpers"""
+
+from .github_service import *
+from .trello_service import *
+
---
+++ gello/services/trello_service.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""TrelloService
+"""
+
+from gello.api_clients import TrelloAPIClient
+
+
+class TrelloService(object):
+    """
+    A class with the single responsibility of interacting with the Trello API
+    """
+
+    def __init__(self):
+        """Initializes a new TrelloService object."""
+        self.api_client = TrelloAPIClient()
+
+    def boards(self):
+        pass
