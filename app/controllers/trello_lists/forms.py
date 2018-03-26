3210c9bded6257f00293ad116d67c3c50748d9bf
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-03-26 15:15:19 -0400

    Added forms to update `List` records.

---
+++ app/controllers/trello_lists/forms.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""lists/forms.py
+
+List-related forms.
+"""
+
+from flask_wtf import Form
+from wtforms import BooleanField, SubmitField
+
+
+class ListForm(Form):
+    """Form for creating a new subscription."""
+    active = BooleanField('Active')
+    submit = SubmitField('Update record')
