9f73b2ceb4e6d46921435242296c4f12d0ca3fdc
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-04-04 14:06:46 -0400

    Created separate `/create` action for subscriptions, reverted back to `root_url` for Github Webhooks.

--- app/controllers/main/views.py
+++ app/controllers/main/views.py
@@ -16,56 +16,24 @@ repos-related routes and view-specific logic.
 """
 
 import json
-import re
-import textwrap
 
-from flask import render_template, request, url_for, flash, redirect
+from flask import render_template, request
 from . import main
-from ...tasks import GitHubReceiver, CreateGitHubWebhook
+from ...tasks import GitHubReceiver
 from ...models import Subscription
-from ...services import SubscriptionService
 from app.controllers.subscriptions.forms import NewSubscriptionForm, \
     UpdateForm, DeleteForm
-
-subscription_service = SubscriptionService()
 
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
     """Updates the repositories saved on POST request."""
     # Creation form logic
+    if request.method == 'POST':
+        GitHubReceiver.delay(payload=json.loads(request.get_data()))
+        return "GitHub event received."
+
     create_form = NewSubscriptionForm()
-    if create_form.validate_on_submit():
-        ids = create_form.list_ids.data
-        list_ids = re.split("\s*,\s*", ids.strip()) if ids else []
-
-        subscription_service.create(
-            board_id=create_form.get_board_id(),
-            repo_id=create_form.get_repo_id(),
-            issue_autocard=create_form.issue_autocard.data,
-            pull_request_autocard=create_form.pull_request_autocard.data,
-            list_ids=list_ids
-        )
-
-        # Enqueue a task to create a repository webhook for the repo
-        CreateGitHubWebhook.delay(
-            url_root=request.url_root,
-            repo_id=create_form.get_repo_id()
-        )
-
-        flash('Created subscription')
-        return redirect(url_for('.index'))
-    elif request.method == 'POST':
-        flash(
-            textwrap.dedent(
-                f"""
-                Could not create subscription because an error occurred:
-                {create_form.get_error_message()}
-                """
-            )
-        )
-        return redirect(url_for('.index'))
-
     page = request.args.get('page', 1, type=int)
     query = Subscription.query
     pagination = query.order_by(Subscription.timestamp.desc()).paginate(
@@ -90,10 +58,3 @@ def index():
         subscription_forms_tuples=subscription_forms_tuples,
         pagination=pagination
     )
-
-
-@main.route('/webhooks', methods=['POST'])
-def webhooks():
-    """Handle GitHub webhooks."""
-    GitHubReceiver.delay(payload=json.loads(request.get_data()))
-    return "GitHub event received."
--- app/controllers/subscriptions/views.py
+++ app/controllers/subscriptions/views.py
@@ -15,14 +15,54 @@
 subscriptions-related routes and view-specific logic.
 """
 
+import re
+import textwrap
 
 from flask import redirect, url_for, flash, request
 from flask_login import login_required
 from . import subscription
-from .forms import UpdateForm
+from .forms import NewSubscriptionForm, UpdateForm
 from ...services import SubscriptionService
+from ...tasks import CreateGitHubWebhook
 
 subscription_service = SubscriptionService()
+
+
+@subscription.route('/create', methods=['POST'])
+@login_required
+def create():
+    create_form = NewSubscriptionForm(request.form)
+
+    if create_form.validate():
+        ids = create_form.list_ids.data
+        list_ids = re.split("\s*,\s*", ids.strip()) if ids else []
+
+        subscription_service.create(
+            board_id=create_form.get_board_id(),
+            repo_id=create_form.get_repo_id(),
+            issue_autocard=create_form.issue_autocard.data,
+            pull_request_autocard=create_form.pull_request_autocard.data,
+            list_ids=list_ids
+        )
+
+        # Enqueue a task to create a repository webhook for the repo
+        CreateGitHubWebhook.delay(
+            url_root=request.url_root,
+            repo_id=create_form.get_repo_id()
+        )
+
+        flash('Created subscription')
+        return redirect(url_for('main.index'))
+    else:
+        flash(
+            textwrap.dedent(
+                f"""
+                Could not create subscription because an error occurred:
+                {create_form.get_error_message()}
+                """
+            )
+        )
+        return redirect(url_for('main.index'))
 
 
 @subscription.route('/<string:board_id>/<int:repo_id>/update', methods=['POST'])
