affd8c1e65435817689985a79c0d2f399754d8da
paullegranddc <paul.legranddescloizeaux@datadoghq.com>
2021-06-30 11:06:13 +0200

    shoul ndbe pull_request instead of issue

--- app/tasks/update_jira_pull_request_issue_labels.py
+++ app/tasks/update_jira_pull_request_issue_labels.py
@@ -47,4 +47,4 @@ class UpdateJiraPullRequestIssueLabels(GitHubBaseTask):
         self.payload = payload
         self.set_scope_data()
         pull_request = PullRequest.query.filter_by(jira_project_key=project_key, github_pull_request_id=pull_request_id).first()
-        self._jira_service.update_issue_labels(project_key=project_key, issue_key=issue.jira_issue_key, label_names=label_names)
+        self._jira_service.update_issue_labels(project_key=project_key, issue_key=pull_request.jira_issue_key, label_names=label_names)
