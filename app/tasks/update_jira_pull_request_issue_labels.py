4c9116c460ea837612be17ecaa791f7bf23c9077
Eric allen <eric.allen@datadoghq.com>
2022-12-05 10:59:18 -0500

    fix: switch to hasattr checks

--- app/tasks/update_jira_pull_request_issue_labels.py
+++ app/tasks/update_jira_pull_request_issue_labels.py
@@ -48,5 +48,5 @@ class UpdateJiraPullRequestIssueLabels(GitHubBaseTask):
         self.set_scope_data()
         pull_request = PullRequest.query.filter_by(jira_project_key=project_key, github_pull_request_id=pull_request_id).first()
 
-        if (pull_request.jira_issue_key):
+        if hasattr(pull_request, 'jira_issue_key'):
             self._jira_service.update_issue_labels(project_key=project_key, issue_key=pull_request.jira_issue_key, label_names=label_names)
